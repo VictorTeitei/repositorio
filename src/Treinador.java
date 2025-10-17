3e44f93a7c34726bbf47ba2c9c217b904efe8088
VictorTeitei <victor75008@gmail.com>
2025-10-17 10:46:32 -0300

    Atualizado classes Atleta e Treinador

--- src/Treinador.java
+++ src/Treinador.java
@@ -1,4 +1,12 @@
 
 public class Treinador {
+	
+	String especialidade;
+	
+	public Treinador(int idPar, String nomePar, int idadePar, String especialidadePar){
+		super(idPar, nomePar, idadePar);
+		
+		this.especialidade = especialidadePar;
+	}
 
 }
