04b64ba960b7f96e5b3e12f43a33a4b2c7e1955e
VictorTeitei <victor75008@gmail.com>
2025-10-17 10:42:01 -0300

    Atualizado classes Atleta e Pessoa

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,4 +1,17 @@
 
-public class Atleta {
-
+public class Atleta extends Pessoa{
+	String modalidade;
+	
+	public Atleta(int idPar, String nomePar, int idadePar, String modalidadePar){
+		super(idPar, nomePar, idadePar);
+		this.modalidade = modalidadePar;
+	}
+	
+	public String getModalidade(){
+		return modalidade;
+	}
+	
+	public void setModalidade(String modalidadePar){
+		this.modalidade = modalidade;
+	}
 }
