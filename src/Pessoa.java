18b1a5a4f3aff038da10152cd6e4e15a0ec75267
VictorTeitei <victor75008@gmail.com>
2025-10-17 09:38:13 -0300

    Atualizado classes

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,42 @@
 
 public class Pessoa {
 
+	private int idAt;
+	private String nomeAt:
+	private int idadeAt;
+	
+	public Pessoa(){
+		
+	}
+	
+	public Pessoa(int idPar, String nomePar, int idadePar){
+		this.idAt = idPar;
+		this.nomeAt = nomePar;
+		this.idadeAt = idadePar;
+	}
+	
+	public int getId(){
+		return idAt;
+	}
+	
+	public String getNome(){
+		return nomeAt;
+	}
+	
+	public int getIdade(){
+		return idadeAt;
+	}
+	
+	public void setId(int id){
+		this.idAt = id;
+		
+	}
+	
+	public void setNome(String nome){
+		this.nomeAt = nome;
+	}
+	
+	public void setIdade(int idade){
+		this.idadeAt = idade;
+	}
 }
